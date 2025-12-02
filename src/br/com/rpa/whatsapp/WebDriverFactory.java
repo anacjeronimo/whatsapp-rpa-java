f4174fb8185e6a2952a2bf06610a729716807285
Ana Carolina Jerônimo <61793024+anacjeronimo@users.noreply.github.com>
2025-12-02 08:52:38 -0300

    Create WebDriverFactory for ChromeDriver setup

---
+++ src/br/com/rpa/whatsapp/WebDriverFactory.java
@@ -0,0 +1,15 @@
+package br.com.rpa.whatsapp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebDriverFactory {
+
+    public static WebDriver createDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
+        return new ChromeDriver(options);
+    }
+}
